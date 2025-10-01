7aefed58281cae24707c6d7b943b9d8ada7f1245
Fondue28 <nattasasi.fong@gmail.com>
2025-10-01 19:29:54 +0700

    first commit

---
+++ models/Category.js
@@ -0,0 +1,15 @@
+import { MoneyOffCsredRounded } from "@mui/icons-material";
+import mongoose from "mongoose";
+
+const categorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  order: Number
+});
+
+const Category = mongoose.models.category || mongoose.model("category", categorySchema);
+
+export default Category;
+//define data model 
